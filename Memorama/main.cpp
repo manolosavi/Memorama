34684d2e77ab90987e4d2983bd33a454dd015c1f
manolo <manolosavi@me.com>
2016-02-06 14:13:41 -0600

    Initial Commit

---
+++ Memorama/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Memorama
+//
+//  Created by manolo on 2/6/16.
+//  Copyright © 2016 manolo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
